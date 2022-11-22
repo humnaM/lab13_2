ed340e5da6dbcedded330155096976b20fcfa2a0
humnaM <113585041+humnaM@users.noreply.github.com>
2022-11-22 23:05:09 +0500

    Add files via upload

---
+++ i192166_1.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+class Workshop{
+	public:
+		virtual void getWorkshop(){
+			cout<<"hello i am workshop"<<endl;
+		}
+	
+};
+
+class produce: public Workshop{
+	public:
+		void getWorkshop(){
+			cout<<"hello i am workshop"<<endl;
+		}
+		
+};
+
+class assemble: public Workshop{
+	public:
+		void getWorkshop(){
+			cout<<"hello i am workshop"<<endl;
+		}
+		
+	
+};
+
+class Vehicle{
+	public:
+		produce pro;
+		assemble assem;
+		virtual void getVehicle(){
+			cout<<"hello i am vehicle"<<endl;
+		}
+		
+	
+
+};
+
+class bike: public Vehicle{
+	public:
+		void getVehicle(){
+			cout<<"hello i am vehicle"<<endl;
+		}
+};
+
+class bus: public Vehicle{
+	public:
+		void getVehicle(){
+			cout<<"hello i am vehicle"<<endl;
+		}
+};
+
+
+int main(){
+	
+	bike bik;
+	bik.pro.getWorkshop();
+	bik.assem.getWorkshop();
+	
+	cout<<endl;
+	cout<<endl;
+	
+	bus bu;
+	bu.assem.getWorkshop();
+	bu.pro.getWorkshop();
+	
+	
+	
+	
+}
+
+
+
